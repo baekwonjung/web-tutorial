f26f470064b6694b3d6ea65345dbd8fb4549c313
baekwonjung <bwj9127@dgist.ac.kr>
2022-03-03 16:44:06 +0900

    첫번째

---
+++ abc.js
@@ -0,0 +1,17 @@
+let word = "안녕"
+
+function search(){
+    alert(word)
+}
+
+function change(){
+    word=document.getElementById("word1").value
+}
+
+function plus(){
+    alert(Number(word) ** 2)
+}
+
+// const 상수
+// let b=1
+// b=2
---
+++ plus.js
@@ -0,0 +1,5 @@
+
+
+function plus(){
+    alert(Number(document.getElementById("num1").value)+Number(num2=document.getElementById("num2").value))
+}
